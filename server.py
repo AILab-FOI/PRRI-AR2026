5f93d7d5449d4a14183273a459869050114809be
lbalazine21 <lbalazine21@foi.hr>
2024-05-12 22:28:43 +0200

    Implemented working models

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
 from http.server import HTTPServer, SimpleHTTPRequestHandler 
 import ssl
+
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+
 httpd = HTTPServer(('0.0.0.0', 443), SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(
-    httpd.socket,
-    keyfile="key.pem",
-    certfile='cert.pem',
-    server_side=True)
+
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
 httpd.serve_forever()
