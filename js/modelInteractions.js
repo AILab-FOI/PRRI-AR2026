f7c5a02e9d750d0c11307b4b9e209f9f5364dd6b
lcavar21 <lcavar21@foi.hr>
2024-06-17 18:57:20 +0200

    Implemented do not show feature on puzzle hints

--- js/modelInteractions.js
+++ js/modelInteractions.js
@@ -1,9 +1,15 @@
+let markerStatus = {
+  "headphonesMarker": false,
+  "radioMarker": false,
+  "kalkulatorMarker": false
+};
 
 document.addEventListener('DOMContentLoaded', (event) => {
   const headphonesMarker = document.querySelector('a-marker[type="pattern"][url="Patterns/Headphones_pattern.patt"]');
   const radioMarker = document.querySelector('a-marker[type="pattern"][url="Patterns/Radio_pattern.patt"]');
-  const kalkulatorMarker= document.querySelector('a-marker[type="pattern"][url="Patterns/Kalkulator_pattern.patt"]');
+  const kalkulatorMarker = document.querySelector('a-marker[type="pattern"][url="Patterns/Calculator_pattern.patt"]');
   headphonesMarker.addEventListener('markerFound', function () {
+    if (markerStatus["headphonesMarker"]) return;
 
     Swal.fire({
       position: 'top-start',
@@ -14,7 +20,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
       confirmButtonColor: '#4CAF50',
       cancelButtonColor: '#4CAF50',
       confirmButtonText: 'Ok',
-      cancelButtonText: 'Cancel'
+      cancelButtonText: 'Do not show again'
     }).then((result) => {
       if (result.isConfirmed) {
         fetch('/scan', {
@@ -32,13 +38,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
           .catch(error => {
             console.error(error);
           });
+      } else if (result.isDismissed) {
+        markerStatus["headphonesMarker"] = true;
       }
     })
-
-
   });
 
   radioMarker.addEventListener('markerFound', function () {
+    if (markerStatus["radioMarker"]) return;
     Swal.fire({
       position: 'top-start',
       title: 'Puzzle 1-2',
@@ -47,7 +54,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
       confirmButtonColor: '#4CAF50',
       cancelButtonColor: '#4CAF50',
       confirmButtonText: 'Ok',
-      cancelButtonText: 'Cancel'
+      cancelButtonText: 'Do not show again'
     }).then((result) => {
       if (result.isConfirmed) {
         fetch('/scan', {
@@ -66,9 +73,52 @@ document.addEventListener('DOMContentLoaded', (event) => {
             console.error(error);
           });
       }
+      else if (result.isDismissed) {
+        markerStatus["radioMarker"] = true;
+      }
     })
 
   });
+
+  kalkulatorMarker.addEventListener('markerFound', function () {
+    if (markerStatus["kalkulatorMarker"]) return;
+    Swal.fire({
+      position: 'top-start',
+      icon: 'question',
+      title: 'Puzzle 3',
+      text: "A + B + C = ?",
+      showCancelButton: true,
+      confirmButtonColor: '#4CAF50',
+      cancelButtonColor: '#4CAF50',
+      confirmButtonText: 'Ok',
+      cancelButtonText: 'Do not show again'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch('/scan', {
+          method: 'POST',
+          body: JSON.stringify({ marker_id: "kalkulator", player: 'player1' }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+
+        })
+          .then(response => response.json())
+          .then(data => {
+            // Handle the response from the backend
+            console.log(data);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+      else if (result.isDismissed) {
+        markerStatus["kalkulatorMarker"] = true;
+      }
+    })
+
+
+  }
+  );
 });
 
 //kada se marker skenira šalje se potvrda skena na beckend
